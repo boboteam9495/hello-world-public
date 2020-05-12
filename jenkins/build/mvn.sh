df2b3be3bbb85ef004b4d2df6e386ce3a9c04571
jenkins <jenkins@192.168.0.110>
2020-05-13 00:46:23 +0100

    change to jenkins workspace

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run -v $PWD/java-app:/app -v $HOME/.m2:/root/.m2 -v $PWD/java-app/target:/usr/src/mymaven/target -w /app maven:3-alpine "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+docker run -v $WORKSPACE/java-app:/app -v $HOME/.m2:/root/.m2 -v $PWD/java-app/target:/usr/src/mymaven/target -w /app maven:3-alpine "$@"
