83235a8fb8b20a35c0c059f4f37dc967b171f916
jenkins <jenkins@192.168.0.110>
2020-05-13 01:01:37 +0100

    change directory

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-
-rm -r jenkins/build/*.jar
-
-
 echo "***************************"
 echo "*******Build jar***********"
+echo "***************************"
 
-jenkins/build/mvn.sh mvn -B -DskipTests package
 
-cp java-app/target/*.jar jenkins/build/
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+$WORKSPACE/jenkins/build/mvn.sh mvn -B -DskipTests package
+
+cp $WORKSPACE/java-app/target/*.jar jenkins/build/
 
 cd jenkins/build && docker-compose -f docker-compose-build.yml up
