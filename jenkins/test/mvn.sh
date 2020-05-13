20e52a5bf8fa35a252a2da2e1534e1416c7b9ada
jenkins <jenkins@192.168.0.110>
2020-05-13 01:32:02 +0100

    add test directory

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-docker run -v $PWD/java-app:/app -v $HOME/.m2:/root/.m2 -v $PWD/java-app/target:/usr/src/mymaven/target -w /app maven:3-alpine "$@"
+WORKSPACE=/home/jenkins/jenkins-data/jenkins_home/workspace/pipeline-docker-maven
+
+
+docker run -v $WORKSPACE/java-app:/app -v $HOME/.m2:/root/.m2 -v $WORKSPACE/java-app/target:/usr/src/mymaven/target -w /app maven:3-alpine "$@"
 
 cp java-app/target/surefire-reports/TEST-com.mycompany.app.AppTest.xml jenkins/test/test-report.xml
